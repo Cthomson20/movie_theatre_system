b147e6608df7ef5f79e09c1a5329539f0e17a659
am1rawish <amira.wishah@ucalgary.ca>
2025-11-19 01:00:27 -0700

    working on building showtimes page structure further

--- src/ShowtimesPage.jsx
+++ src/ShowtimesPage.jsx
@@ -18,39 +18,98 @@ function ShowtimesPage() {
               {/* Location + date filters */}
       <div className="filters-row">
         <select className="filter-select">
-          <option>CINENOVA MARKET MALL</option>
+          <option>CineNova Market Mall</option>
         </select>
 
         <select className="filter-select">
-          <option>TODAY</option>
+          <option>Today</option>
         </select>
       </div>
 
             {/* REGULAR row */}
       <section className="showtime-section">
-        <div className="format-row">
-          <span className="format-name">REGULAR</span>
-          <span className="format-badge">CC</span>
-        </div>
+        <div className="format-and-times">
 
-        <div className="times-row">
-          <button className="time-button">
-            <span className="time-label">10:00</span>
-            <img src="src/assets/seat_icon.png" className="time-seat-icon" />
+          <div className="format-row">
+            <span className="format-name">REGULAR</span>
+            <span className="format-badge">CC</span>
+          </div>
 
-          </button>
+          <div className="times-row">
+            <button className="time-button">
+              <span className="time-label">10:00</span>
+              <img src="src/assets/seat_icon.png" className="time-seat-icon" />
 
-          <button className="time-button">
-            <span className="time-label">12:30</span>
-            <img src="src/assets/seat_icon.png" className="time-seat-icon" />
-          </button>
+            </button>
 
-          <button className="time-button">
-            <span className="time-label">17:15</span>
-            <img src="src/assets/seat_icon.png" className="time-seat-icon" />
-          </button>
+            <button className="time-button">
+              <span className="time-label">12:30</span>
+              <img src="src/assets/seat_icon.png" className="time-seat-icon" />
+            </button>
+
+            <button className="time-button">
+              <span className="time-label">17:15</span>
+              <img src="src/assets/seat_icon.png" className="time-seat-icon" />
+            </button>
+          </div>
+
         </div>
       </section>
+
+    
+    <section className="showtime-section">
+        <div className="format-and-times">
+
+          <div className="format-row">
+            <span className="format-name">VIP</span>
+            <span className="format-badge">CC</span>
+          </div>
+
+          <div className="times-row">
+            <button className="time-button">
+              <span className="time-label">18:20</span>
+              <img src="src/assets/seat_icon.png" className="time-seat-icon" />
+
+            </button>
+
+            <button className="time-button">
+              <span className="time-label">21:00</span>
+              <img src="src/assets/seat_icon.png" className="time-seat-icon" />
+            </button>
+
+          </div>
+        </div>
+      </section>
+
+
+
+
+
+      <section className="showtime-section">
+        <div className="format-and-times">
+
+          <div className="format-row">
+            <span className="format-name">ULTRA AVX <br />DOLBY ATMOS</span>
+            <span className="format-badge">CC</span>
+          </div>
+
+          <div className="times-row">
+            <button className="time-button">
+              <span className="time-label">14:10</span>
+              <img src="src/assets/seat_icon.png" className="time-seat-icon" />
+
+            </button>
+
+            <button className="time-button">
+              <span className="time-label">20:30</span>
+              <img src="src/assets/seat_icon.png" className="time-seat-icon" />
+            </button>
+          </div>
+
+        </div>
+      </section>
+
+      
 
 
     </div>
